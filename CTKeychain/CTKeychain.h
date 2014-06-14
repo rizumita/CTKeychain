23a005bcc2970d5d9e80472ec862970a1c6e8f7c
Ryoichi Izumita <r.izumita@caph.jp>
2014-06-14 21:32:12 +0900

    Initial Commit

---
+++ CTKeychain/CTKeychain.h
@@ -0,0 +1,19 @@
+//
+//  CTKeychain.h
+//  CTKeychain
+//
+//  Created by 和泉田 領一 on 2014/06/14.
+//  Copyright (c) 2014年 CAPH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CTKeychain.
+FOUNDATION_EXPORT double CTKeychainVersionNumber;
+
+//! Project version string for CTKeychain.
+FOUNDATION_EXPORT const unsigned char CTKeychainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTKeychain/PublicHeader.h>
+
+
